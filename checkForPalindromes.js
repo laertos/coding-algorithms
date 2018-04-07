6464e95ffafbfda63ca0079b4b342b7f333afd52
laertos <laertalickolli@gmail.com>
2018-04-06 22:16:57 -0500

    function that checks if a complicated string is a plndrm

---
+++ checkForPalindromes.js
@@ -0,0 +1,18 @@
+//Return true if the given string is a palindrome. Otherwise, return false
+
+
+function palindrome(str) {
+  // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+
+  
+// Step 2. Use the same chaining methods with built-in functions from the previous article 'Three Ways to Reverse a String in JavaScript'
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+
+   
+  // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+  return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+}
+ 
+palindrome("A man, a plan, a canal. Panama");
