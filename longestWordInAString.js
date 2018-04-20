1c66d9256f3d4c2d23d4e57e8ea69d4a82e68628
laertos <laertalickolli@gmail.com>
2018-04-19 23:53:56 -0500

    algorithm to find the longest word in a given string

---
+++ longestWordInAString.js
@@ -0,0 +1,14 @@
+//Return the length of the longest word in the provided sentence.
+
+
+function findLongestWord(str) {
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
